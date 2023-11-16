b78b1c7b1a1bb5fc53b56dac0aa13c64cf64458e
JOSEPH IZUU <eeagleyee@gmail.com>
2023-11-16 17:17:35 +0100

    second commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Avatar,
   Button,
@@ -7,11 +8,12 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import React from "react";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 
+// Login component definition
 const Login = () => {
+  // Styles for elements within the component
   const paperStyle = {
     padding: 20,
     height: "70vh",
@@ -20,20 +22,28 @@ const Login = () => {
   };
   const avatarStyle = { backgroundColor: "cadetblue" };
   const btnStyle = { margin: "10px 0" };
+
+  // Render the login form
   return (
     <Grid>
+      {/* Paper component to contain the login form */}
       <Paper elevation={10} style={paperStyle}>
+        {/* Grid for aligning elements in the center */}
         <Grid align={"center"}>
+          {/* Avatar for login icon */}
           <Avatar style={avatarStyle} src="/broken-image.jpg" />
+          {/* Heading for sign in */}
           <h4>Sign In</h4>
         </Grid>
+        {/* Username input field */}
         <TextField
           label="Username"
           placeholder="Enter Username"
           fullWidth
           required
-          style={{ marginBottom: "20px" }}
+          style={{ marginBottom: "20px" }} // Style for space after username field
         />
+        {/* Password input field */}
         <TextField
           label="Password"
           placeholder="Enter Password"
@@ -41,10 +51,12 @@ const Login = () => {
           fullWidth
           required
         />
+        {/* Checkbox for 'Remember me' */}
         <FormControlLabel
-          control={<Checkbox defaultChecked />}
+          control={<Checkbox defaultChecked />} // Checkbox with default checked state
           label="Remember me"
         />
+        {/* Sign in button */}
         <Button
           type="submit"
           fullWidth
@@ -54,9 +66,11 @@ const Login = () => {
         >
           Sign In
         </Button>
+        {/* Forgot password link */}
         <Typography>
           <Link href="#">Forgot Password?</Link>
         </Typography>
+        {/* Sign up link */}
         <Typography>
           {"Do you have an account? "}
           <Link href="#">Sign Up</Link>
@@ -66,4 +80,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Login; // Export the Login component
